3d61cc9bc200822171c1cbe68077e5d9a02bf8ed
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-10-29 17:23:19 +0700

    feat: remove unused button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
 export default function Home() {
-  return <main>homepage</main>;
+  return (
+    <main>
+      <Button onClick={() => signIn()}>Log in</Button>
+      <br />
+      <Link href={"/dashboard"} className="underline">
+        Dashboard
+      </Link>
+    </main>
+  );
 }
