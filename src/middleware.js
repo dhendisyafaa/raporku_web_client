619a3582367baa5b7f64a06bfe23ace1762dd66c
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-10-31 13:39:07 +0700

    feat(next-auth): add next auth for authentication user

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import withAuth from "./middlewares/withAuth";
+
+export function mainMiddleware(req) {
+  const res = NextResponse.next();
+  return res;
+}
+
+export default withAuth(mainMiddleware, ["/dashboard"]);
---
+++ src/middlewares/withAuth.js
@@ -0,0 +1,20 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export default function withAuth(middleware, requireAuth) {
+  return async (req, next) => {
+    const pathname = req.nextUrl.pathname;
+    if (requireAuth.includes(pathname)) {
+      const token = await getToken({
+        req,
+        secret: process.env.NEXTAUTH_SECRET,
+      });
+      if (!token) {
+        const url = new URL("/auth/login", req.url);
+        url.searchParams.set("callbackUrl", encodeURI(req.url));
+        return NextResponse.redirect(url);
+      }
+    }
+    return middleware(req, next);
+  };
+}
