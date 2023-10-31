7a2402760cdb8f8a2fc2eeab06e9aa49a95263a2
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-10-31 13:33:43 +0700

    feat: wrap component using SessionProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 export default function App({
   Component,
@@ -7,15 +10,17 @@ export default function App({
 }) {
   return (
     <>
-      <NextNProgress
-        color="#dc2626"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-        showOnShallow={true}
-        options={{ easing: "ease", speed: 500, showSpinner: false }}
-      />
-      <Component {...pageProps} />;
+      <SessionProvider session={session}>
+        <NextNProgress
+          color="#dc2626"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+          options={{ easing: "ease", speed: 500, showSpinner: false }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
