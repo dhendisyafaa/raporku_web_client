fe6971de4d390193fc481c5a58ac50a9d480f434
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2023-12-02 15:04:24 +0700

    feat: add setup for tanstask query

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import axios from "axios";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
-import NextTopLoader from "nextjs-toploader";
+import NextNProgress from "nextjs-progressbar";
+import { useState } from "react";
 
 axios.defaults.withCredentials = true;
 
@@ -10,6 +13,7 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <>
       <Head>
@@ -27,22 +31,20 @@ export default function App({
         <title>Raporku</title>
       </Head>
       <main>
-        <SessionProvider session={session}>
-          <NextTopLoader
-            color="#FE5E44"
-            initialPosition={0.3}
-            crawlSpeed={500}
-            height={3}
-            crawl={true}
-            showSpinner={false}
-            easing="ease"
-            speed={500}
-            shadow="0 0 10px #FE5E44,0 0 5px #FE5E44"
-            zIndex={1600}
-            showAtBottom={false}
-          />
-          <Component {...pageProps} />
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider session={session}>
+            <NextNProgress
+              color="#FE5E44"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+              showOnShallow={true}
+              options={{ easing: "ease", speed: 500, showSpinner: false }}
+            />
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Component {...pageProps} />
+          </SessionProvider>
+        </QueryClientProvider>
       </main>
     </>
   );
